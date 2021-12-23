df7b3894e3a06ba2dbfff5f798576ff8f9124c06
hector franco <hector.franco.aceituno@gmail.com>
2021-12-23 19:16:32 +0100

    updated

---
+++ 04.controladores/08.ejemplo_cors/symfony/src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\AbstractController;
+use ProxyManager\Factory\RemoteObject\Adapter\JsonRpc;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/default', name: 'default')]
+    public function index(): Response
+    {
+        $this->enableCors();
+        // 
+        $result = ['result' => ['first test Ecors']];
+
+        return new JsonResponse($result);
+    }
+}
